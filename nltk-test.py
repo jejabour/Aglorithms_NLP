5d71c5cd7bc82e4f72610937c453a6b6fac81ece
jejabour <jejabour@gmail.com>
2023-11-16 21:47:57 -0600

    made some work

--- nltk-test.py
+++ nltk-test.py
@@ -14,8 +14,56 @@ import os
 import pandas as pd
 import numpy as np
 import gensim
-
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
+
+
+
+
+path = 'test-txt-search/'
+token_dict = {}
+stemmer = PorterStemmer()
+
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
+
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
+    stems = stem_tokens(tokens, stemmer)
+    return stems
+
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        print(file)
+        file_path = subdir + os.path.sep + file
+        shakes = open(file_path, 'r')
+        text = shakes.read()
+        lowers = text.lower()
+        translating = str.maketrans('', '', string.punctuation)
+        no_punctuation = lowers.translate(translating)
+        token_dict[file] = no_punctuation
+        
+#this can take some time
+tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+tfs = tfidf.fit_transform(token_dict.values())
+
+print(tfidf)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -43,22 +91,28 @@ from nltk.tokenize import word_tokenize
 
 # path = "./test-txt-search/"
 
-
-
-
 # file with coating
 # test-txt-search/1-s2.0-S8756328205000050-main.txt
 
 # first file
 #  "test-txt-search/1-s2.0-S875632820800197X-main.txt"
 
-file_docs = []
+# file_docs = []
 
-with open ('test-doc.txt', encoding='utf-8') as f:
-    tokens = sent_tokenize(f.read())
-    for line in tokens:
-        file_docs.append(line)
+# with open ('test-doc.txt', encoding='utf-8') as f:
+#     tokens = sent_tokenize(f.read())
+#     # for line in tokens:
+#     #     file_docs.append(line)
 
+
+# with open ('test-doc.txt', encoding='utf-8') as f:
+#     words = word_tokenize(f.read())
+
+# print("Tokens: ", type(tokens))
+# print("Words: ", type(words))
+# print(type(tokens))
+# print()
+# print(type(file_docs))
 # print("Number of documents:",len(file_docs))
 
 
@@ -66,36 +120,62 @@ with open ('test-doc.txt', encoding='utf-8') as f:
 # I'll keep the original code above the changes
 # with open('test-doc.txt', 'r', encoding="utf-8") as f:
 #     tokens = word_tokenize(f.read())
-
 #     for line in tokens:
 #         file_docs.append(line)
         
 
-gen_docs = [[w.lower() for w in word_tokenize(text)]
-            for text in file_docs]
+# gen_docs = [[w.lower() for w in word_tokenize(text)]
+#             for text in tokens]
 
-# gen_docs = [w.lower() for w in file_docs]
+# gen_docss = [wl.lower() for wl in words]
+
+# print()
+# print("gendocs: ", gen_docs)
+# print()
+# print("gendocss: ", gen_docss)
+
+# gen_docss = [gen_docss]
+# print(gen_docs)
+# print()
+
 
 # This allows the change and it's kinda dumb. Dictionary needs it to be an array of arrays
 # which would normnally happen when we do sent_tokenize and then word_tokenize
 # but I only wanted word_tokenize
-gen_docss = [gen_docs]
+# gen_docss = [gen_docss]
+# # print(gen_docss)
 
 
-dictionary = gensim.corpora.Dictionary(gen_docs)
-print(dictionary)
-# dictionary = gensim.corpora.Dictionary(gen_docss)
+# dictionary = gensim.corpora.Dictionary(gen_docs)
+# dictionary2 = gensim.corpora.Dictionary(gen_docss)
+# # print(dictionary)
+# print()
+# print("Dictionary2")
+# print(dictionary2)
+# # dictionary = gensim.corpora.Dictionary(gen_docss)
 
 
-corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
-# corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docss]
+# corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docs]
+# corpus2 = [dictionary2.doc2bow(gen_doc2) for gen_doc2 in gen_docss]
+# # corpus = [dictionary.doc2bow(gen_doc) for gen_doc in gen_docss]
+# print("corpus")
 # print(corpus)
+# print()
+# print("Corpus2")
+# print(corpus2)
 
 
+# print()
+# print("tf_idf")
+# tf_idf = gensim.models.TfidfModel(corpus)
+# for doc in tf_idf[corpus]:
+#     print([[dictionary[id], np.around(freq, decimals=2)] for id, freq in doc])
 
-tf_idf = gensim.models.TfidfModel(corpus)
-for doc in tf_idf[corpus]:
-    print([[dictionary[id], np.around(freq, decimals=2)] for id, freq in doc])
+# print()
+# print("tf_idf2")
+# tf_idf2 = gensim.models.TfidfModel(corpus2)
+# for doc in tf_idf2[corpus2]:
+#     print([[dictionary[id], np.around(freq, decimals=2)] for id, freq in doc])
 
 # sims = gensim.similarities.Similarity('workdir/', tf_idf[corpus], num_features=len(dictionary))
 
